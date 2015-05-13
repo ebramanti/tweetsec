f4c980ccef6234cf03246619d7b4636a7345f212
Edward Bramanti <edward@bramanti.org>
2015-05-13 13:39:21 -0700

    Password received from tweet

--- server.js
+++ server.js
@@ -4,4 +4,19 @@ var utils = require('./utils');
 var config = require('./config');
 
 var Bot = new Twit(config);
+Bot.botname = '@' + config.botUsername;
 console.log("Working...");
+
+var stream = Bot.stream('statuses/filter', {
+    track: Bot.botname
+});
+
+stream.on('tweet', function(tweet) {
+    formatTweet(tweet);
+});
+
+function formatTweet(tweet) {
+    var user = tweet.user.screen_name;
+    var password = tweet.text.replace(Bot.botname + ' ', '');
+    console.log(password);
+}
