0ae4ebf58cf14b3703700ceed45834d0acedcd4a
Edward Bramanti <edward@bramanti.org>
2015-05-13 11:45:38 -0700

    Find number of character types with tests

--- test/utils-test.js
+++ test/utils-test.js
@@ -2,6 +2,14 @@ var assert = require('assert');
 var utils = require('../utils');
 
 describe('Utils', function() {
+    it('should return correct number of character types', function() {
+        var a = "password1";
+        assert.equal(utils.getTypes(a), 2);
+        var b = "goat m4n";
+        assert.equal(utils.getTypes(b), 3);
+        var c = "s0_0per 5nak3";
+        assert.equal(utils.getTypes(c), 4);
+    });
     it('should return expected values', function() {
         var a = "password1";
         assert.equal(utils.calculateScore(a), 4);
--- utils.js
+++ utils.js
@@ -1,5 +1,35 @@
+
+
 var utils = module.exports = {
     calculateScore: function(password) {
         var result = 0;
+        var updatedtext = '';
+        //Replace English words
+
+        //Find character types in string
+        var types = this.getTypes(updatedtext);
+        //Multiply character types by updated length
+
+        return result;
+    },
+
+    getTypes: function(string) {
+        return this.hasLetters(string) + this.hasDigits(string) + this.hasWhitespace(string) + this.hasMisc(string);
+    },
+
+    hasWhitespace: function(str) {
+        return (/\s+/g.test(str)) ? 1 : 0;
+    },
+
+    hasLetters: function(str) {
+        return (/[A-Za-z]+/g.test(str)) ? 1 : 0;
+    },
+
+    hasDigits: function(str) {
+        return (/[0-9]+/g.test(str)) ? 1 : 0;
+    },
+
+    hasMisc: function(str) {
+        return (/[^A-Za-z0-9\s]+/g.test(str)) ? 1 : 0;
     }
 }
